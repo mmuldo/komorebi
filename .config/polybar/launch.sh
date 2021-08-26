1d7f0eafaeb780130dd4b7577e8cc2ddc42c9b18
Matt Muldowney <matt.muldowney@gmail.com>
2021-08-26 17:31:19 -0500

    final commit

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,8 +4,13 @@
 killall -q polybar
 sleep 1
 
-# launch bars
-polybar -r top &
-polybar -r bottom &
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+	MONITOR=$m polybar -r top &
+	MONITOR=$m polybar -r bottom &
+done
 
-echo "bars launched"
+# launch bars
+# polybar -r top &
+# polybar -r bottom &
+
+notify-send "bars launched"
